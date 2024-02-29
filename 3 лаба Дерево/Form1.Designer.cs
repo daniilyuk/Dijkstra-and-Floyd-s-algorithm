d7d04a90593df40c692b5fae4ed640b3fb912a8f
Your Name <you@example.com>
2024-02-29 13:13:38 +0300

    Добавьте файлы проекта.

---
+++ 3 лаба Дерево/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _3_лаба_Дерево
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDijkstra = new System.Windows.Forms.Button();
+            this.btnFloyd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDijkstra
+            // 
+            this.btnDijkstra.Location = new System.Drawing.Point(536, 71);
+            this.btnDijkstra.Name = "btnDijkstra";
+            this.btnDijkstra.Size = new System.Drawing.Size(75, 23);
+            this.btnDijkstra.TabIndex = 0;
+            this.btnDijkstra.Text = "button1";
+            this.btnDijkstra.UseVisualStyleBackColor = true;
+            this.btnDijkstra.Click += new System.EventHandler(this.btnDijkstra_Click);
+            // 
+            // btnFloyd
+            // 
+            this.btnFloyd.Location = new System.Drawing.Point(536, 156);
+            this.btnFloyd.Name = "btnFloyd";
+            this.btnFloyd.Size = new System.Drawing.Size(75, 23);
+            this.btnFloyd.TabIndex = 1;
+            this.btnFloyd.Text = "button2";
+            this.btnFloyd.UseVisualStyleBackColor = true;
+            this.btnFloyd.Click += new System.EventHandler(this.btnFloyd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFloyd);
+            this.Controls.Add(this.btnDijkstra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDijkstra;
+        private System.Windows.Forms.Button btnFloyd;
+    }
+}
+
---
+++ 3 лаба Дерево/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_лаба_Дерево
+{
+    public partial class Form1 : Form
+    {
+        private int[,] graph = {
+            { 0, 4, 0, 0, 0, 0, 0, 3 },
+            { 4, 0, 0, 3, 0, 6, 0, 2 },
+            { 0, 0, 0, 5, 0, 0, 4, 0 },
+            { 0, 3, 5, 0, 2, 3, 0, 0 },
+            { 0, 0, 0, 2, 0, 5, 0, 0 },
+            { 0, 6, 0, 3, 5, 0, 0, 2 },
+            { 0, 0, 4, 0, 0, 0, 0, 5 },
+            { 3, 2, 0, 0, 0, 2, 5, 0 }
+        };
+        private const int Infinity = int.MaxValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] Dijkstra(int[,] graph, int startVertex)
+        {
+            int verticesCount = graph.GetLength(0);
+            int[] distances = new int[verticesCount];
+            bool[] visited = new bool[verticesCount];
+
+            // Инициализация расстояний до всех вершин как "бесконечность",
+            // кроме начальной вершины, расстояние до которой равно 0
+            for (int i = 0; i < verticesCount; i++)
+            {
+                distances[i] = Infinity;
+            }
+            distances[startVertex] = 0;
+
+            for (int i = 0; i < verticesCount - 1; i++)
+            {
+                int minDistance = Infinity;
+                int nearestVertex = -1;
+
+                // Находим ближайшую непосещенную вершину
+                for (int vertex = 0; vertex < verticesCount; vertex++)
+                {
+                    if (!visited[vertex] && distances[vertex] < minDistance)
+                    {
+                        minDistance = distances[vertex];
+                        nearestVertex = vertex;
+                    }
+                }
+
+                // Помечаем вершину как посещенную
+                visited[nearestVertex] = true;
+
+                // Обновляем расстояния до смежных вершин
+                for (int vertex = 0; vertex < verticesCount; vertex++)
+                {
+                    int edgeWeight = graph[nearestVertex, vertex];
+                    if (edgeWeight > 0 && minDistance + edgeWeight < distances[vertex])
+                    {
+                        distances[vertex] = minDistance + edgeWeight;
+                    }
+                }
+            }
+
+            return distances;
+        }
+
+        private int[,] Floyd(int[,] graph)
+        {
+            int verticesCount = graph.GetLength(0);
+            int[,] distances = new int[verticesCount, verticesCount];
+
+            // Инициализация матрицы расстояний
+            for (int i = 0; i < verticesCount; i++)
+            {
+                for (int j = 0; j < verticesCount; j++)
+                {
+                    distances[i, j] = graph[i, j];
+                    if (i != j && distances[i, j] == 0)
+                    {
+                        distances[i, j] = Infinity;
+                    }
+                }
+            }
+
+            // Алгоритм Флойда
+            for (int k = 0; k < verticesCount; k++)
+            {
+                for (int i = 0; i < verticesCount; i++)
+                {
+                    for (int j = 0; j < verticesCount; j++)
+                    {
+                        if (distances[i, k] != Infinity && distances[k, j] != Infinity &&
+                            distances[i, k] + distances[k, j] < distances[i, j])
+                        {
+                            distances[i, j] = distances[i, k] + distances[k, j];
+                        }
+                    }
+                }
+            }
+
+            return distances;
+        }
+
+        private void DisplayResults(int[] distances)
+        {
+            string result = "Кратчайшие расстояния от города 1:\n";
+            for (int i = 0; i < distances.Length; i++)
+            {
+                result += $"До города {i + 1}: {(distances[i] == Infinity ? "Недостижим" : distances[i].ToString())}\n";
+            }
+            MessageBox.Show(result);
+        }
+
+        private void btnFloyd_Click(object sender, EventArgs e)
+        {
+            int[,] distances = Floyd(graph);
+            int startVertex = 0; // Начальная вершина (город Оли)
+
+            // Получаем расстояния от начальной вершины до всех остальных
+            int[] shortestDistances = new int[graph.GetLength(0)];
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                shortestDistances[i] = distances[startVertex, i];
+            }
+
+            DisplayResults(shortestDistances);
+        }
+
+        private void btnDijkstra_Click(object sender, EventArgs e)
+        {
+            int startVertex = 0; // Начальная вершина (город Оли)
+            int[] distances = Dijkstra(graph, startVertex);
+
+            DisplayResults(distances);
+        }
+    }
+}
