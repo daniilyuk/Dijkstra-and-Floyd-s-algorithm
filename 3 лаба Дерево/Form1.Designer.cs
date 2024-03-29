6497e52ab93af0944879de617015241b25043333
Daniil Antonov <daniilantonov406@gmail.com>
2024-03-29 12:33:05 +0300

    Added algorithm for passing all vertices

--- 3 лаба Дерево/Form1.Designer.cs
+++ 3 лаба Дерево/Form1.Designer.cs
@@ -28,47 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnDijkstra = new System.Windows.Forms.Button();
-            this.btnFloyd = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnDijkstra
+            // pictureBox1
             // 
-            this.btnDijkstra.Location = new System.Drawing.Point(536, 71);
-            this.btnDijkstra.Name = "btnDijkstra";
-            this.btnDijkstra.Size = new System.Drawing.Size(75, 23);
-            this.btnDijkstra.TabIndex = 0;
-            this.btnDijkstra.Text = "button1";
-            this.btnDijkstra.UseVisualStyleBackColor = true;
-            this.btnDijkstra.Click += new System.EventHandler(this.btnDijkstra_Click);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 280);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
-            // btnFloyd
+            // button3
             // 
-            this.btnFloyd.Location = new System.Drawing.Point(536, 156);
-            this.btnFloyd.Name = "btnFloyd";
-            this.btnFloyd.Size = new System.Drawing.Size(75, 23);
-            this.btnFloyd.TabIndex = 1;
-            this.btnFloyd.Text = "button2";
-            this.btnFloyd.UseVisualStyleBackColor = true;
-            this.btnFloyd.Click += new System.EventHandler(this.btnFloyd_Click);
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(22, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(311, 55);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Найти оптимальный путь";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(651, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Количество шагов:";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Количество шагов:";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(707, 37);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 67);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Алгоритм Флойда";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 37);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 67);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Алгоритм Дейкстры";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(366, 130);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 180);
+            this.listBox1.TabIndex = 16;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(654, 130);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(222, 180);
+            this.listBox2.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnFloyd);
-            this.Controls.Add(this.btnDijkstra);
+            this.ClientSize = new System.Drawing.Size(1012, 424);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnDijkstra;
-        private System.Windows.Forms.Button btnFloyd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- 3 лаба Дерево/Form1.cs
+++ 3 лаба Дерево/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,137 +13,258 @@ namespace _3_лаба_Дерево
 {
     public partial class Form1 : Form
     {
-        private int[,] graph = {
-            { 0, 4, 0, 0, 0, 0, 0, 3 },
-            { 4, 0, 0, 3, 0, 6, 0, 2 },
-            { 0, 0, 0, 5, 0, 0, 4, 0 },
-            { 0, 3, 5, 0, 2, 3, 0, 0 },
-            { 0, 0, 0, 2, 0, 5, 0, 0 },
-            { 0, 6, 0, 3, 5, 0, 0, 2 },
-            { 0, 0, 4, 0, 0, 0, 0, 5 },
-            { 3, 2, 0, 0, 0, 2, 5, 0 }
-        };
-        private const int Infinity = int.MaxValue;
-
         public Form1()
         {
             InitializeComponent();
+            graphOfStrotestsWays = graph;
         }
+        public static int countDejkstra = 0;
+        public static int countFloyd = 0;
+        public static int N = 8;
+        public double[,] graph = new double[8, 8]
+            {
+                { 0, I, I, I, I, I, I, 2 },
+                { I, 0, I, 3, I, 6, I, 2 },
+                { I, I, 0, 5, I, I, 4, I },
+                { I, 3, 5, 0, 2, 3, I, I },
+                { I, I, I, 2, 0, 5, I, I },
+                { I, 6, I, 3, 5, 0, I, 5 },
+                { I, I, 4, I, I, I, 0, 5 },
+                { 2, 2, I, I, I, 5, 5, 0 }
+             };
 
-        private int[] Dijkstra(int[,] graph, int startVertex)
+        public double[,] graphOfStrotestsWays;
+        public double[,] graphOfIntermediateVertices = new double[N, N];
+        public static double I = double.PositiveInfinity;
+        public static int[,] visitedVertices = new int[N, N];
+
+        public static Dictionary<string, double> DijkstraAlgorithm(Dictionary<string, Dictionary<string, int>> graph, string start)
         {
-            int verticesCount = graph.GetLength(0);
-            int[] distances = new int[verticesCount];
-            bool[] visited = new bool[verticesCount];
-
-            // Инициализация расстояний до всех вершин как "бесконечность",
-            // кроме начальной вершины, расстояние до которой равно 0
-            for (int i = 0; i < verticesCount; i++)
+            var distances = new Dictionary<string, double>();
+            foreach (var vertex in graph.Keys)
             {
-                distances[i] = Infinity;
-            }
-            distances[startVertex] = 0;
-
-            for (int i = 0; i < verticesCount - 1; i++)
-            {
-                int minDistance = Infinity;
-                int nearestVertex = -1;
-
-                // Находим ближайшую непосещенную вершину
-                for (int vertex = 0; vertex < verticesCount; vertex++)
-                {
-                    if (!visited[vertex] && distances[vertex] < minDistance)
-                    {
-                        minDistance = distances[vertex];
-                        nearestVertex = vertex;
-                    }
-                }
-
-                // Помечаем вершину как посещенную
-                visited[nearestVertex] = true;
-
-                // Обновляем расстояния до смежных вершин
-                for (int vertex = 0; vertex < verticesCount; vertex++)
-                {
-                    int edgeWeight = graph[nearestVertex, vertex];
-                    if (edgeWeight > 0 && minDistance + edgeWeight < distances[vertex])
-                    {
-                        distances[vertex] = minDistance + edgeWeight;
-                    }
-                }
+                distances[vertex] = double.PositiveInfinity;
             }
 
+            distances[start] = 0;
+            Tuple<double, string> start_point = new Tuple<double, string>(0, start);
+            countDejkstra++;
+            List<Tuple<double, string>> priorityQueue = new List<Tuple<double, string>>() { start_point };
+            while (priorityQueue.Count > 0)
+            {
+                Tuple<double, string> lowest_vertex = priorityQueue[0];
+                for (int i = 1; i < priorityQueue.Count; i++)
+                {
+                    if (priorityQueue[i].Item1 < lowest_vertex.Item1)
+                    {
+                        lowest_vertex = priorityQueue[i];
+                    }
+                }
+                priorityQueue.Remove(lowest_vertex);
+                var (currentDistance, currentVertex) = lowest_vertex;
+
+                if (currentDistance > distances[currentVertex])
+                {
+                    continue;
+                }
+
+                foreach (var neighbor in graph[currentVertex])
+                {
+                    var weight = neighbor.Value;
+                    var distance = currentDistance + weight;
+
+                    if (distance < distances[neighbor.Key])
+                    {
+                        countDejkstra++;
+                        distances[neighbor.Key] = distance;
+                        priorityQueue.Add(new Tuple<double, string>(distance, neighbor.Key));
+                    }
+                }
+            }
             return distances;
         }
 
-        private int[,] Floyd(int[,] graph)
+        public List<List<int>> Permutations(List<int> lst, int k)
         {
-            int verticesCount = graph.GetLength(0);
-            int[,] distances = new int[verticesCount, verticesCount];
-
-            // Инициализация матрицы расстояний
-            for (int i = 0; i < verticesCount; i++)
+            if (k == 0)
             {
-                for (int j = 0; j < verticesCount; j++)
+                return new List<List<int>> { new List<int>() };
+            }
+            List<List<int>> result = new List<List<int>>();
+            for (int i = 0; i < lst.Count; i++)
+            {
+                List<int> temp_lst = new List<int>(lst);
+                temp_lst.RemoveAt(i);
+                List<List<int>> temp_permutations = Permutations(temp_lst, k - 1);
+                foreach (var perm in temp_permutations)
                 {
-                    distances[i, j] = graph[i, j];
-                    if (i != j && distances[i, j] == 0)
+                    perm.Insert(0, lst[i]);
+                    result.Add(perm);
+                }
+            }
+            return result;
+        }
+        public static void GenerateCombination(int[] nums, int[] temp, int index, int start)
+        {
+            if (index >= temp.Length)
+            {
+                ;
+            }
+            else
+            {
+                for (int i = start; i < nums.Length; i++)
+                {
+                    temp[index] = nums[i];
+                    GenerateCombination(nums, temp, index + 1, i);
+                }
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var graph = new Dictionary<string, Dictionary<string, int>>
+            {
+                { "1", new Dictionary<string, int> { { "2", 4 }, { "8", 3 } } },
+                { "2", new Dictionary<string, int> { { "1", 4 }, { "4", 3 }, { "6", 6 }, { "8", 2 } } },
+                { "3", new Dictionary<string, int> { { "4", 5 }, { "7", 4 } } },
+                { "4", new Dictionary<string, int> { { "2", 3 }, { "3", 5 }, { "5", 2 }, { "6", 3 } } },
+                { "5", new Dictionary<string, int> { { "4", 2 }, { "6", 5 } } },
+                { "6", new Dictionary<string, int> { { "2", 6 }, { "4", 3 }, { "5", 5 }, { "8", 2 } } },
+                { "7", new Dictionary<string, int> { { "3", 4 }, { "8", 5 } } },
+                { "8", new Dictionary<string, int> { { "1", 3 }, { "2", 2 }, { "6", 2 }, { "7", 5 } } }
+            };
+            Dictionary<string, double> result = DijkstraAlgorithm(graph, "1");
+
+            foreach (var person in result)
+            {
+                listBox1.Items.Add($"Путь до {person.Key} вершины: {person.Value}");
+            }
+
+            label1.Visible = true;
+            label1.Text = label1.Text + countDejkstra;
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            var graph = new Dictionary<string, Dictionary<string, int>>
+            {
+                { "1", new Dictionary<string, int> { { "2", 4 }, { "8", 3 } } },
+                { "2", new Dictionary<string, int> { { "1", 4 }, { "4", 3 }, { "6", 6 }, { "8", 2 } } },
+                { "3", new Dictionary<string, int> { { "4", 5 }, { "7", 4 } } },
+                { "4", new Dictionary<string, int> { { "2", 3 }, { "3", 5 }, { "5", 2 }, { "6", 3 } } },
+                { "5", new Dictionary<string, int> { { "4", 2 }, { "6", 5 } } },
+                { "6", new Dictionary<string, int> { { "2", 6 }, { "4", 3 }, { "5", 5 }, { "8", 2 } } },
+                { "7", new Dictionary<string, int> { { "3", 4 }, { "8", 5 } } },
+                { "8", new Dictionary<string, int> { { "1", 3 }, { "2", 2 }, { "6", 2 }, { "7", 5 } } }
+            };
+            Dictionary<string, double> result = DijkstraAlgorithm(graph, "1");
+
+            foreach (var person in result)
+            {
+                listBox2.Items.Add($"Путь до {person.Key} вершины: {person.Value}");
+            }
+
+            label2.Visible = true;
+            label2.Text = label2.Text+ 512.ToString();
+
+
+            for (int u = 0; u < N; u++)
+            {
+                for (int v = 0; v < N; v++)
+                {
+                    graphOfIntermediateVertices[u, v] = v;
+                }
+            }
+
+            for (int k = 0; k < N; k++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
                     {
-                        distances[i, j] = Infinity;
+                        double d = graphOfStrotestsWays[i, k] + graphOfStrotestsWays[k, j];
+                        if (graphOfStrotestsWays[i, j] > d && visitedVertices[i, k] < 2 && visitedVertices[k, j] < 2)
+                        {
+                            graphOfStrotestsWays[i, j] = d;
+                            graphOfIntermediateVertices[i, j] = k;
+                            visitedVertices[i, k]++;
+                            visitedVertices[k, j]++;
+                        }
+                        countFloyd++;
                     }
                 }
             }
 
-            // Алгоритм Флойда
-            for (int k = 0; k < verticesCount; k++)
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            List<int> friends = new List<int>();
+            for (int i = 1; i < N; i++)
             {
-                for (int i = 0; i < verticesCount; i++)
+                friends.Add(i);
+            }
+            List<List<int>> permutations = new List<List<int>>();
+            foreach (var p in Permutations(friends, N - 1))
+            {
+                List<int> list = new List<int>() { 0 };
+                list.AddRange(p);
+                list.Add(0);
+                permutations.Add(list);
+            }
+          
+            double min_len_path = double.PositiveInfinity;
+            List<int> min_path = new List<int>();
+            foreach (var permutation in permutations)
+            {
+                List<int> path = new List<int>() { 0 };
+                permutation.RemoveAt(0);
+                foreach (var vertex in permutation)
                 {
-                    for (int j = 0; j < verticesCount; j++)
+                    var next = Convert.ToInt32(graphOfIntermediateVertices[path[path.Count() - 1], vertex]);
+                    if (graph[path[path.Count() - 1], next] == I)
                     {
-                        if (distances[i, k] != Infinity && distances[k, j] != Infinity &&
-                            distances[i, k] + distances[k, j] < distances[i, j])
+                        var row = new List<double>();
+                        for (int i = 0; i < N; i++)
                         {
-                            distances[i, j] = distances[i, k] + distances[k, j];
+                            row.Add(graph[path[path.Count() - 1], i]);
+                        }
+                        for (int i = 0; i < row.Count; i++)
+                        {
+                            if (row[i] != I && row[i] != 0)
+                            {
+                                next = i;
+                            }
                         }
                     }
+                    path.Add(next);
+                    while (vertex != next)
+                    {
+                        next = Convert.ToInt32(graphOfIntermediateVertices[path[path.Count() - 1], vertex]);
+                        path.Add(next);
+                    }
+                }
+                double current_len_path = 0;
+                for (int vertex = 0; vertex < path.Count - 1; vertex++)
+                {
+                    int current_vertex = path[vertex];
+                    int next_vertex = path[vertex + 1];
+                    current_len_path += graphOfStrotestsWays[current_vertex, next_vertex];
+                }
+                if (current_len_path < min_len_path)
+                {
+                    min_len_path = current_len_path;
+                    min_path = path;
                 }
             }
-
-            return distances;
-        }
-
-        private void DisplayResults(int[] distances)
-        {
-            string result = "Кратчайшие расстояния от города 1:\n";
-            for (int i = 0; i < distances.Length; i++)
+            string rez = "";
+            for (int i = 0; i < min_path.Count; i++)
             {
-                result += $"До города {i + 1}: {(distances[i] == Infinity ? "Недостижим" : distances[i].ToString())}\n";
+                rez += (min_path[i] + 1).ToString();
             }
-            MessageBox.Show(result);
-        }
+            MessageBox.Show($"Минимальный путь = {min_len_path}\nПуть = {rez}");
 
-        private void btnFloyd_Click(object sender, EventArgs e)
-        {
-            int[,] distances = Floyd(graph);
-            int startVertex = 0; // Начальная вершина (город Оли)
-
-            // Получаем расстояния от начальной вершины до всех остальных
-            int[] shortestDistances = new int[graph.GetLength(0)];
-            for (int i = 0; i < graph.GetLength(0); i++)
-            {
-                shortestDistances[i] = distances[startVertex, i];
-            }
-
-            DisplayResults(shortestDistances);
-        }
-
-        private void btnDijkstra_Click(object sender, EventArgs e)
-        {
-            int startVertex = 0; // Начальная вершина (город Оли)
-            int[] distances = Dijkstra(graph, startVertex);
-
-            DisplayResults(distances);
         }
     }
 }
